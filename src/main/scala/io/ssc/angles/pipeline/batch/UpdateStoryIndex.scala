b4f948c3ab429a2042f2a4886909cfd9484d6f43
Sam Beyond <sam.beyond@octopost.eu>
2015-02-06 09:18:21 +0100

    re-add lost license text

--- src/main/scala/io/ssc/angles/pipeline/batch/UpdateStoryIndex.scala
+++ src/main/scala/io/ssc/angles/pipeline/batch/UpdateStoryIndex.scala
@@ -1,4 +1,20 @@
-
+/**
+* Angles
+* Copyright (C) 2014  Sebastian Schelter
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package io.ssc.angles.pipeline.batch
 
 import io.ssc.angles.pipeline._
@@ -14,6 +30,8 @@ object UpdateStoryIndex extends App {
 
   val work = new Queue[Step]()
   work.enqueue(new FetchTimelines)
+  work.enqueue(new FetchRetweets)
+  work.enqueue(new FetchExplorers)
   work.enqueue(new CrawlUris)
   work.enqueue(new ExtractMetadata)
   work.enqueue(new ExtractNamedEntities)
