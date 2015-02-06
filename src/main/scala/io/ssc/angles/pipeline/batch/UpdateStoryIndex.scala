7063bd56152cea48bd788db81fcd1f4cece07991
Sam Beyond <sam.beyond@octopost.eu>
2015-02-06 09:15:49 +0100

    retry when rate limit is exceeded

--- src/main/scala/io/ssc/angles/pipeline/batch/UpdateStoryIndex.scala
+++ src/main/scala/io/ssc/angles/pipeline/batch/UpdateStoryIndex.scala
@@ -15,22 +15,45 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package io.ssc.angles.pipeline.batch
 
 import io.ssc.angles.pipeline._
 import org.joda.time.DateTime
+import org.slf4j.LoggerFactory
+import twitter4j.TwitterException
 
+import scala.collection.mutable.Queue
 
 object UpdateStoryIndex extends App {
+  val log = LoggerFactory.getLogger(UpdateStoryIndex.getClass)
+
+  val work = new Queue[Step]()
+  work.enqueue(new FetchTimelines)
+  work.enqueue(new FetchRetweets)
+  work.enqueue(new FetchExplorers)
+  work.enqueue(new CrawlUris)
+  work.enqueue(new ExtractMetadata)
+  work.enqueue(new ExtractNamedEntities)
+  work.enqueue(new IndexArticles)
 
   val since = new DateTime().minusDays(5)
+  do {
+    try {
+      val step = work.dequeue()
+      step.execute(since)
+    } catch {
+      case tex: TwitterException => {
+        if (tex.exceededRateLimitation()) {
+          val limit = tex.getRateLimitStatus
 
-  new FetchTimelines().execute(since)
-  new CrawlUris().execute(since)
-
-  new ExtractMetadata().execute(since)
-  new ExtractNamedEntities().execute(since)
-
-  new IndexArticles().execute(since)
+          val sleepTime = limit.getSecondsUntilReset + 1
+          log.info("Rate limit exceeded, going to sleep for {} seconds and then try again", sleepTime)
+          Thread.sleep(sleepTime * 1000)
+        } else {
+          // Ugh?!
+          log.error("{}", tex)
+        }
+      }
+    }
+  }  while (!work.isEmpty)
 }
