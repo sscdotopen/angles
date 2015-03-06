4b09db77cc51fe48412afa64313f008639b3c8a4
xolor <gebratene.ente+github@gmail.com>
2015-03-06 16:09:51 +0000

    Add explorerName to graph

--- src/main/scala/io/ssc/angles/pipeline/titan/LoadGraphTitan.scala
+++ src/main/scala/io/ssc/angles/pipeline/titan/LoadGraphTitan.scala
@@ -10,7 +10,7 @@ import com.thinkaurelius.titan.core.attribute.Precision
 import com.thinkaurelius.titan.core.util.TitanCleanup
 import com.tinkerpop.blueprints.util.wrappers.batch.BatchGraph
 import com.tinkerpop.blueprints.{Direction, Edge, TransactionalGraph, Vertex}
-import io.ssc.angles.pipeline.explorers.CSVReader
+import io.ssc.angles.pipeline.explorers.{HelperUtils, CSVReader}
 import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConversions._
@@ -94,10 +94,14 @@ object LoadGraphTitan extends App {
     mgmt.buildIndex("byExplorerId", classOf[Vertex]).addKey(explorerId).unique().buildCompositeIndex()
     // Mixed vertex index on property "explorerName"
     val explorerName = mgmt.makePropertyKey("explorerName").dataType(classOf[String]).make()
-    mgmt.buildIndex("byExplorerName", classOf[Vertex]).addKey(explorerName).buildMixedIndex(INDEX_BACKEND)
-    // Mixed vertex index on property "url"
+    mgmt.buildIndex("explorerName_search", classOf[Vertex]).addKey(explorerName).buildMixedIndex(INDEX_BACKEND)
+    // Composite vertex index on property "explorerName"
+    mgmt.buildIndex("explorerName", classOf[Vertex]).addKey(explorerName).buildCompositeIndex()
+    // Mixed vertex index on property "host"
     val url = mgmt.makePropertyKey("host").dataType(classOf[String]).make()
-    mgmt.buildIndex("byHost", classOf[Vertex]).addKey(url).buildMixedIndex(INDEX_BACKEND)
+    mgmt.buildIndex("host_search", classOf[Vertex]).addKey(url).buildMixedIndex(INDEX_BACKEND)
+    // Mixed vertex index on property "host"
+    mgmt.buildIndex("host", classOf[Vertex]).addKey(url).buildCompositeIndex()
     // Composite vertex index on property "vertexKey"
     val vertexKey = mgmt.makePropertyKey("key").dataType(classOf[String]).make()
     mgmt.buildIndex("key", classOf[Vertex]).addKey(vertexKey).buildCompositeIndex()
@@ -123,7 +127,8 @@ object LoadGraphTitan extends App {
     workingList.par.foreach { case triple => {
       try {
         val explorerId = triple._1
-        val uri = URI.create(triple._3)
+        val uriTemp: String = HelperUtils.escapeURIString(triple._3)
+        val uri = URI.create(uriTemp)
         var newValue: util.List[String] = explorerUrlMap.getOrElse(explorerId, Collections.synchronizedList(new util.ArrayList[String]()))
         newValue += uri.getHost
         explorerUrlMap.update(explorerId, newValue)
@@ -132,7 +137,7 @@ object LoadGraphTitan extends App {
       }
     }
     }
-
+    val explorerIdNameMap: Map[String, String] = workingList.par.map((t : Tuple3[String, String, String]) => (t._1, t._2)).toMap.seq
     val explorerHostCountMap: mutable.Map[String, Map[String, Int]] = explorerUrlMap.map((s: (String, util.List[String])) => (s._1, s._2.groupBy(identity).mapValues(_.size)))
     val vertexIdMap = new util.HashMap[String, String]()
 
@@ -146,6 +151,8 @@ object LoadGraphTitan extends App {
       var vertexId = vertexIdMap.getOrDefault(explorerId, (() => {id += 1; id.toString}).apply())
 
       val explorerNode = graph.addVertex(vertexId, "explorerId", explorerId)
+      explorerNode.setProperty("explorerName", explorerIdNameMap.get(explorerId))
+      
       //explorerNode.setProperty("tweetedUrls", explorerUrlMap.get(explorerId).toArray.asInstanceOf[Array[String]])
 
       urls.foreach {
